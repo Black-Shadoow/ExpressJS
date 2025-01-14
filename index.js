ddb680f7340008829c2b99b1ecc5c2f3a9e2abb4
Dark Shadoow <kunwarbibek66632@gmail.com>
2025-01-14 08:49:12 +0545

    Building Rest API

--- index.js
+++ index.js
@@ -16,10 +16,6 @@ app.get('/hello',(req,res)=>{
     res.send("hello World");
     });
 
-//return json data 
-app.get('/api/user',(req,res)=>{
-    res.json(users);
-    });
 
 // User route to return an HTML list of user first names
 app.get('/user', (req, res) => {
@@ -34,8 +30,49 @@ app.get('/user', (req, res) => {
     res.send(html); // Send the constructed HTML as a response
 });
 
+//working with json data 
+//return json data 
+app.get('/api/user',(req,res)=>{
+    res.json(users);
+    });
 
+// app.get("/api/user/:id",(req,res)=>{ ///api/users/:id  this id is user defined and prams id is same 
+//     const id=Number(req.params.id)
+//     const user = users.find((user) => user.id === id);
+//     return res.json(user);
+// })
+//  app.post("/api/user/",(req,res)=>{
+//     // todo to create a user 
+//     return res.json({status:"Pending"})
+//  })
 
+//  app.patch("/api/user/",(req,res)=>{
+//     // todo to edit a user  with id
+//     return res.json({status:"Pending"})
+//  })
+
+//  app.delete("/api/user/",(req,res)=>{
+//     // todo to Delete a user 
+//     return res.json({status:"Pending"})
+//  })
+
+app.route('/api/user/:id').get((req,res)=>{
+    const id=Number(req.params.id)
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+})
+.post((req,res)=>{
+    // todo to create a user 
+    return res.json({status:"Pending"})
+ })
+.patch((req,res)=>{
+    // todo to edit a user  with id
+    return res.json({status:"Pending"})
+ })
+ .delete((req,res)=>{
+    // todo to Delete a user 
+    return res.json({status:"Pending"})
+ })
 
 app.listen(PORT, () => { 
     console.log(`The code is running on port http://localhost:${PORT}`);
